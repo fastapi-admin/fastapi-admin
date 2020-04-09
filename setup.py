a9b1e40e7c8a411cc7a5f9ccf70403d78db322c8
long2ice <long2ice@prismslight.com>
2020-04-09 10:47:28 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 import os
+import re
 
 from setuptools import find_packages, setup
-from fastapi_admin import __version__
+
+
+def version():
+    ver_str_line = open('fastapi_admin/__init__.py', 'rt').read()
+    mob = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", ver_str_line, re.M)
+    if not mob:
+        raise RuntimeError("Unable to find version string")
+    return mob.group(1)
+
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
     long_description = f.read()
@@ -13,7 +22,7 @@ def requirements():
 
 setup(
     name='fastapi-admin',
-    version=__version__,
+    version=version(),
     description='Fast Admin Dashboard based on fastapi and tortoise-orm and rest-admin.',
     author='long2ice',
     long_description_content_type='text/x-rst',
