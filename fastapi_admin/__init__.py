f592a4288e807588bba26988cb98a6a5d82b391b
long2ice <long2ice@gmail.com>
2020-04-21 18:16:27 +0800

    fix permissions

--- fastapi_admin/__init__.py
+++ fastapi_admin/__init__.py
@@ -1,3 +1,3 @@
 from . import routes
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
--- fastapi_admin/routes/site.py
+++ fastapi_admin/routes/site.py
@@ -27,12 +27,20 @@ async def site(
     if app.permission and not user.is_superuser:
         site_ = deepcopy(site_)
         await user.fetch_related('roles')
-        for menu in filter(lambda x: (x.url and 'rest' in x.url) or x.children, site_.menus):
+        filter_menus = filter(lambda x: (x.url and 'rest' in x.url) or x.children, site_.menus)
+        hide_menus = []
+        for menu in filter_menus:
             has_permission = False
-            if not has_permission:
-                for role in user.roles:
-                    if await role.permissions.filter(model=menu.name):
+            for role in user.roles:
+                if not has_permission:
+                    model = menu.url.split('/')[-1]
+                    permission = await role.permissions.filter(model=model)
+                    if permission:
                         has_permission = True
             if not has_permission:
+                hide_menus.append(menu.url)
+        for menu in app.site.menus:
+            if menu.url in hide_menus:
                 site_.menus.remove(menu)
+
     return site_.dict(by_alias=True, exclude_unset=True)
