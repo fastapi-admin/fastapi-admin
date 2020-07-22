0795627b5d9162f7d9032f82e07192b803fb66dd
long2ice <long2ice@gmail.com>
2020-07-22 18:08:57 +0800

    update deps

--- fastapi_admin/__init__.py
+++ fastapi_admin/__init__.py
@@ -5,4 +5,4 @@ def version():
     # with open("pyproject.toml") as f:
     #     ret = re.findall(r'version = "(\d+\.\d+\.\d+)"', f.read())
     #     return ret[0]
-    return "0.2.9"
+    return "0.3.0"
