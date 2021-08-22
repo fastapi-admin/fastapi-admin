ed17a39738f427d310c8829b769c7861273a69b6
long2ice <long2ice@gmail.com>
2021-08-22 18:09:02 +0800

    Fix example email required

--- examples/models.py
+++ examples/models.py
@@ -8,7 +8,7 @@ from fastapi_admin.models import AbstractAdmin
 
 class Admin(AbstractAdmin):
     last_login = fields.DatetimeField(description="Last Login", default=datetime.datetime.now)
-    email = fields.CharField(max_length=200)
+    email = fields.CharField(max_length=200, default="")
     avatar = fields.CharField(max_length=200, default="")
     intro = fields.TextField(default="")
     created_at = fields.DatetimeField(auto_now_add=True)
