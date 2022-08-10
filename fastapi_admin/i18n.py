e22786407bc160e30c4eb79d557e2285bb9f7074
Ased Mammad <mammad.ased@gmail.com>
2022-08-11 03:04:15 +0430

    Add persian locale

--- fastapi_admin/i18n.py
+++ fastapi_admin/i18n.py
@@ -9,6 +9,7 @@ TRANSLATIONS = {
     "zh_CN": Translations.load(os.path.join(BASE_DIR, "locales"), locales=["zh_CN"]),
     "en_US": Translations.load(os.path.join(BASE_DIR, "locales"), locales=["en_US"]),
     "fr_FR": Translations.load(os.path.join(BASE_DIR, "locales"), locales=["fr_FR"]),
+    "fa_IR": Translations.load(os.path.join(BASE_DIR, "locales"), locales=["fa_IR"]),
 }
 
 translations = TRANSLATIONS.get("en_US")
