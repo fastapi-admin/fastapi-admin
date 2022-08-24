25e4ea24b72beed1c222086aa034d3345a83679b
Jinlong Peng <jinlong.peng@merico.dev>
2022-08-24 20:14:01 +0800

    fix: build

--- build.py
+++ build.py
@@ -1,11 +1,7 @@
 import subprocess
 
 
-def create_mo_files(setup_kwargs):
+def build(setup_kwargs):
     print("Compile translations:")
     subprocess.run(["pybabel", "compile", "--directory", "fastapi_admin/locales"])
     return setup_kwargs
-
-
-if __name__ == "__main__":
-    create_mo_files({})
