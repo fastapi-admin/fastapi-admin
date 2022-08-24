37534cecd0990eb3d9f34e1f5bdc6ff770f6cd76
Jinlong Peng <jinlong.peng@merico.dev>
2022-08-24 21:25:11 +0800

    fix: mo

--- build.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-
-def build(setup_kwargs):
-    print("Compile translations:")
-    subprocess.run(["pybabel", "compile", "--directory", "fastapi_admin/locales"])
-    return setup_kwargs
